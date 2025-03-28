231b275f44a44eb50c867dc183e3ef53b9b3556b
henriqueritter <henrique-ritter@hotmail.com>
2025-03-28 17:03:12 -0300

    chore: add lint and prettier config

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-import Express from 'express';
+import Express from "express";
 
-import { listAllTemplatesService } from './services/listAllTemplatesService.js';
+import { listAllTemplatesService } from "./services/listAllTemplatesService.js";
 
 const app = Express();
 
 app.use(Express.json());
 
-app.post('/api/v1/videos/generate/image-overlay', (request, response) => {
-    return response.send('Not Implemented');
+app.post("/api/v1/videos/generate/image-overlay", (request, response) => {
+    return response.send("Not Implemented");
 });
 
-app.get('/api/v1/videos/:id', (request, response) => {
-    return response.send('OK');
+app.get("/api/v1/videos/:id", (request, response) => {
+    return response.send("OK");
 });
 
-app.get('/api/v1/videos/templates', (request, response) => {
+app.get("/api/v1/videos/templates", (request, response) => {
     const { pageSize, pageNumber } = request.query;
 
     const result = listAllTemplatesService({ pageSize, pageNumber });
