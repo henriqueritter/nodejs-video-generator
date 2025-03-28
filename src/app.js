53eb5265576aaa2b36c54ff7d8243490cd4f2904
henriqueritter <henrique-ritter@hotmail.com>
2025-03-28 17:24:44 -0300

    feat: validation on optional pagination queryParams from templates route

--- src/app.js
+++ src/app.js
@@ -7,19 +7,30 @@ const app = Express();
 app.use(Express.json());
 
 app.post("/api/v1/videos/generate/image-overlay", (request, response) => {
-    return response.send("Not Implemented");
+  return response.send("Not Implemented");
 });
 
-app.get("/api/v1/videos/:id", (request, response) => {
-    return response.send("OK");
+app.get("/api/v1/videos", (request, response) => {
+  return response.send("OK");
 });
 
 app.get("/api/v1/videos/templates", (request, response) => {
-    const { pageSize, pageNumber } = request.query;
+  const { pageSize, pageNumber } = request.query;
 
-    const result = listAllTemplatesService({ pageSize, pageNumber });
+  if (pageSize && pageSize * 1 != pageSize) {
+    return response.status(400).json({
+      error: { message: "o valor do pageSize informado é inválido." },
+    });
+  }
+  if (pageNumber && pageNumber * 1 != pageNumber) {
+    return response.status(400).json({
+      error: { message: "o valor do pageNumber informado é inválido." },
+    });
+  }
 
-    response.json(result);
+  const result = listAllTemplatesService({ pageSize, pageNumber });
+
+  return response.json(result);
 });
 
 export { app };
