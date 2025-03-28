42f53edb15badf80ecc5eccadba74864ec9baa34
henriqueritter <henrique-ritter@hotmail.com>
2025-03-28 15:13:55 -0300

    feat: add GET templates to list all available templates

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 import Express from 'express';
 
+import { listAllTemplatesService } from './services/listAllTemplatesService.js';
+
 const app = Express();
 
 app.use(Express.json());
 
-app.post('/api/v1/videos/generate', (request, response) => {
+app.post('/api/v1/videos/generate/image-overlay', (request, response) => {
     return response.send('Not Implemented');
 });
+
 app.get('/api/v1/videos/:id', (request, response) => {
     return response.send('OK');
+});
+
+app.get('/api/v1/videos/templates', (request, response) => {
+    const { pageSize, pageNumber } = request.query;
+
+    const result = listAllTemplatesService({ pageSize, pageNumber });
+
+    response.json(result);
 });
 
 export { app };
---
+++ src/services/listAllTemplatesService.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+
+const templates = JSON.parse(fs.readFileSync('./src/templates.json', 'utf8'));
+
+function listAllTemplatesService({ pageSize = 100, pageNumber = 1 }) {
+    if (!templates || templates.length === 0) throw new Error("Error getting templates");
+    console.log(templates);
+
+    const totalRecords = templates.length;
+
+    const totalPages = Math.round(Math.max(totalRecords / pageSize, 1));
+
+    const startPageAt = (pageNumber - 1) * pageSize;
+    const endPageAt = pageNumber * pageSize;
+
+    const result = templates.slice(startPageAt, endPageAt);
+
+    return {
+        templates: result,
+        pagination: {
+            pageSize,
+            pageNumber,
+            totalPages,
+            totalRecords
+        }
+    };
+}
+
+export { listAllTemplatesService };
