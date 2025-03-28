c15cfc74d4ea3f2960e880862200ddc0850cff03
henriqueritter <henrique-ritter@hotmail.com>
2025-03-28 12:00:39 -0300

    docs: created Swagger/OpenAPI spec of REST API

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(Express.json());
 app.post('/api/v1/videos/generate', (request, response) => {
     return response.send('Not Implemented');
 });
-app.get('/api/v1/videos/generate', (request, response) => {
+app.get('/api/v1/videos/:id', (request, response) => {
     return response.send('OK');
 });
 
